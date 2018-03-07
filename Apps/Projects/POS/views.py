3652cff3fcf0ad64a0f73df10f5255c58b185650
Mr24 <sys+github@verysvery.com>
2018-03-07 00:25:04 -0800

    MatsuoStation.Python3.Django : Apps.Projects.POS - Ver.3.0.2 Modify
    Changes to be committed:
    	new file:   Projects/POS/urls.py - New.Setup
    	modified:   Projects/POS/views.py
    	modified:   config/urls.py

--- Apps/Projects/POS/views.py
+++ Apps/Projects/POS/views.py
@@ -1,3 +1,17 @@
+#//+------------------------------------------------------------------+
+#//|                          VerysVeryInc.Python3.Django.POS.Wsgi.py |
+#//|                  Copyright(c) 2018, VerysVery Inc. & Yoshio.Mr24 |
+#//|                 https://github.com/MatsuoStation/Python3.Django/ |
+#//|                                                 Since:2018.03.05 |
+#//|                                Released under the Apache license |
+#//|                       https://opensource.org/licenses/Apache-2.0 |
+#//|  "VsV.Python3.Django.POS.Views.py - Ver.3.0.2 Update:2018.03.07" |
+#//+------------------------------------------------------------------+
 from django.shortcuts import render
 
 # Create your views here.
+### MatsuoStation.Com ###
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Hello POS.py. You're at the POS index.")
