e43828e81d4930db4f62fe18e189bad3463481f9
Mr24 <sys+github@verysvery.com>
2018-03-07 15:02:18 -0800

    MatsuoStation.Python3.Django : Devs.config - Ver.1.0.1 Modify
    Changes to be committed:
    	new file:   config/__init__.py
    	new file:   config/settings.py
    	new file:   config/urls.py
    	new file:   config/wsgi.py
    	modified:   manage.py

---
+++ Devs/config/wsgi.py
@@ -0,0 +1,30 @@
+#//+------------------------------------------------------------------+
+#//|                              VerysVeryInc.Python3.Django.Wsgi.py |
+#//|                  Copyright(c) 2018, VerysVery Inc. & Yoshio.Mr24 |
+#//|                 https://github.com/MatsuoStation/Python3.Django/ |
+#//|                                                 Since:2018.03.05 |
+#//|                                Released under the Apache license |
+#//|                       https://opensource.org/licenses/Apache-2.0 |
+#//|       "VsV.Python3.Django.Wsgi.py - Ver.1.0.1 Update:2018.03.08" |
+#//+------------------------------------------------------------------+
+#//|                                           https://qiita.com/aion |
+#//|               https://qiita.com/aion/items/ca375efac5b90deed382/ |
+#//+------------------------------------------------------------------+
+"""
+WSGI config for Devs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+### MatsuoStation.Com ###
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Devs.settings")
+
+application = get_wsgi_application()
