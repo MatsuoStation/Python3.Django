b209a50e73b17a60998e6c30d098415ef64383a5
Mr24 <sys+github@verysvery.com>
2018-03-08 01:37:51 -0800

    MatsuoStation.Python3.Django : Apps.Projects.Finance - Ver.3.1.2 Modify
    Changes to be committed:
    	new file:   Projects/Finance/urls.py
    	modified:   Projects/Finance/views.py
    	modified:   config/urls.py

--- Apps/Projects/Finance/urls.py
+++ Apps/Projects/Finance/urls.py
@@ -1,11 +1,11 @@
 #//+------------------------------------------------------------------+
-#//|                       VerysVeryInc.Python3.Django.config.urls.py |
+#//|                      VerysVeryInc.Python3.Django.Finance.urls.py |
 #//|                  Copyright(c) 2018, VerysVery Inc. & Yoshio.Mr24 |
 #//|                 https://github.com/MatsuoStation/Python3.Django/ |
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|"VsV.Python3.Django.config.urls.py - Ver.3.1.2 Update:2018.03.08" |
+#//|   "VsV.Python3.Dj.Finance.urls.py - Ver.3.1.2 Update:2018.03.08" |
 #//+------------------------------------------------------------------+
 """Apps URL Configuration
 
@@ -22,17 +22,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-# (Def.OK) from django.urls import path
+from django.urls import path
 
-### MatsuoStatino.Com ###
-from django.urls import include, path
+### MatsuoStation.Com ###
+from . import views
 
 urlpatterns = [
-	### MatsuoStation.Com ###
-	path('', include('Projects.Index.urls')),
-    path('Finance/', include('Projects.Finance.urls')),
-
-	### Config ###
-	path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
--- Apps/Projects/Finance/views.py
+++ Apps/Projects/Finance/views.py
@@ -1,3 +1,17 @@
+#//+------------------------------------------------------------------+
+#//|                     VerysVeryInc.Python3.Django.Finance.Views.py |
+#//|                  Copyright(c) 2018, VerysVery Inc. & Yoshio.Mr24 |
+#//|                 https://github.com/MatsuoStation/Python3.Django/ |
+#//|                                                 Since:2018.03.05 |
+#//|                                Released under the Apache license |
+#//|                       https://opensource.org/licenses/Apache-2.0 |
+#//|  "VsV.Python3.Dj.Finance.Views.py - Ver.3.1.2 Update:2018.03.08" |
+#//+------------------------------------------------------------------+
 from django.shortcuts import render
 
 # Create your views here.
+### MatsuoStation.Com ###
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Apps.Finance Page!! Welcome to Apps.MatsuoStation.Com!")
