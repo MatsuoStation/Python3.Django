0dbb165a8e1429fb4dae1ecbdfa6b8c39515eed5
Mr24 <sys+github@verysvery.com>
2018-03-08 13:20:37 -0800

    MatsuoStation.Python3.Django : Devs.Projects.Invoice - Ver.3.2.2 Modify
    Changes to be committed:
    	new file:   Projects/Invoice/__init__.py
    	new file:   Projects/Invoice/admin.py
    	new file:   Projects/Invoice/apps.py
    	new file:   Projects/Invoice/migrations/__init__.py
    	new file:   Projects/Invoice/models.py
    	new file:   Projects/Invoice/tests.py
    	new file:   Projects/Invoice/urls.py
    	new file:   Projects/Invoice/views.py
    	modified:   config/urls.py

--- Devs/Projects/Invoice/urls.py
+++ Devs/Projects/Invoice/urls.py
@@ -1,11 +1,11 @@
 #//+------------------------------------------------------------------+
-#//|                       VerysVeryInc.Python3.Django.config.urls.py |
+#//|                      VerysVeryInc.Python3.Django.Invoice.urls.py |
 #//|                  Copyright(c) 2018, VerysVery Inc. & Yoshio.Mr24 |
 #//|                 https://github.com/MatsuoStation/Python3.Django/ |
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|"VsV.Python3.Django.config.urls.py - Ver.3.2.2 Update:2018.03.09" |
+#//|   "VsV.Python3.Dj.Invoice.urls.py - Ver.3.2.2 Update:2018.03.09" |
 #//+------------------------------------------------------------------+
 """Devs URL Configuration
 
@@ -22,19 +22,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-# (Def.OK) from django.urls import path
+from django.urls import path
 
-### MatsuoStatino.Com ###
-from django.urls import include, path
+### MatsuoStation.Com ###
+from . import views
 
 urlpatterns = [
-	### MatsuoStation.Com ###
-	path('', include('Projects.Index.urls')),
-    path('Finance/', include('Projects.Finance.urls')),
-	path('Finance/Invoice/', include('Projects.Invoice.urls')),
-    path('LPG/', include('Projects.LPG.urls')),
-
-	### Config ###
-	path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
---
+++ Devs/Projects/Invoice/views.py
@@ -0,0 +1,17 @@
+#//+------------------------------------------------------------------+
+#//|                     VerysVeryInc.Python3.Django.Invoice.Views.py |
+#//|                  Copyright(c) 2018, VerysVery Inc. & Yoshio.Mr24 |
+#//|                 https://github.com/MatsuoStation/Python3.Django/ |
+#//|                                                 Since:2018.03.05 |
+#//|                                Released under the Apache license |
+#//|                       https://opensource.org/licenses/Apache-2.0 |
+#//|  "VsV.Python3.Dj.Invoice.Views.py - Ver.3.2.2 Update:2018.03.09" |
+#//+------------------------------------------------------------------+
+from django.shortcuts import render
+
+# Create your views here.
+### MatsuoStation.Com ###
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Invoice Page!! Welcome to Devs.MatsuoStation.Com!")
