ff7fe4c4585392143a21227fbfcc89a4838323fd
Mr24 <sys+github@verysvery.com>
2018-03-07 17:12:06 -0800

    MatsuoStation.Python3.Django : Apps.config - Ver.1.0.1 Modify
    Changes to be committed:
    	new file:   config/__init__.py
    	new file:   config/settings.py
    	new file:   config/urls.py
    	new file:   config/wsgi.py
    	modified:   manage.py

---
+++ Apps/config/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for Apps project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+### MatsuoStation.Com ###
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Apps.settings")
+
+application = get_wsgi_application()
