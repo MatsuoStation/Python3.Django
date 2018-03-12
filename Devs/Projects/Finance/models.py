fdb5a07107c0d9a7dd89119cae87461530c9f80f
Mr24 <sys+github@verysvery.com>
2018-03-11 23:35:37 -0700

    MatsuoStation.Python3.Django : Devs.Finance.Models.Guest.Address_Test - Ver.3.3.5 Modify
    Changes to be committed:
    	modified:   Projects/Finance/models.py

--- Devs/Projects/Finance/models.py
+++ Devs/Projects/Finance/models.py
@@ -5,13 +5,26 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//| "VsV.Python3.Dj.Finance.Models.py - Ver.3.3.4 Update:2018.03.12" |
+#//| "VsV.Python3.Dj.Finance.Models.py - Ver.3.3.5 Update:2018.03.12" |
 #//+------------------------------------------------------------------+
 from django.db import models
 
 # Create your models here.
 ### MatsuoStation.Com ###
-###* Guest.Name *###
+###* Guest.Address *###
+class Add_Test(models.Model):
+	class Meta:
+		db_table = 'Address_Test'
+		verbose_name = 'Address_000'
+		verbose_name_plural = verbose_name
+		ordering = ['-uid']
+	id = models.AutoField( "id", primary_key=True )
+	uid  = models.CharField( "共通ID", default=None, max_length=16 )
+	postal_code = models.CharField( "郵便番号", default=None, max_length=16 )
+	address = models.CharField( "住所", default=None, max_length=255 )
+
+
+###* Guest.Bank *###
 class Bank_Test(models.Model):
 	class Meta:
 		db_table = 'Bank_Test'
