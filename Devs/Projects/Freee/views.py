e69bf255116ee6d3cbfbcac33162b91fab763b65
VsV_MacBookPro2.9 <vsv_macbookpro2.9@VsV-MacBookPro29.local>
2019-09-02 14:42:46 +0900

    MatsuoSt.Py3.Dj : Devs.Freee.Py.Uriage_List.Views - Ver.3.20.5 Modify
    Changes to be committed:
    	modified:   Projects/Freee/views.py
    	modified:   templates/uriage.html
    	modified:   templates/uriage_list.html

--- Devs/Projects/Freee/views.py
+++ Devs/Projects/Freee/views.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|       "VsV.Py3.Dj.Freee.Views.py - Ver.3.20.4 Update:2019.09.02" |
+#//|       "VsV.Py3.Dj.Freee.Views.py - Ver.3.20.5 Update:2019.09.02" |
 #//+------------------------------------------------------------------+
 # rom django.shortcuts import render
 from django.shortcuts import get_object_or_404, render, redirect
@@ -19,16 +19,35 @@ from django.views.generic import ListView
 from Finance.models import Name_Test20
 from .forms import YearForm
 
-### SS_List ###
+### Uriage_List ###
 class Uriage_List(ListView):
 
 	model = Name_Test20
-	# form_class = NameForm
+	form_class = YearForm
 	template_name = 'uriage_list.html'
 	context_object_name = "nametb"
 	paginate_by = 10
 
+	def post(self, request, *args, **kwargs):
+		form = self.form_class(request.POST)
+		yid_post = request.POST['yid']
 
+		if form.is_valid():
+			return HttpResponseRedirect( '/Freee/Uriage/%s' % yid_post )
+
+		return render(request, self.template_name, {'form': form})
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		context['form'] = YearForm()
+		yid = self.kwargs.get('yid')
+		context['yid'] = yid
+
+		return context
+
+
+### Uriage ###
 def Uriage(request):
 	# return HttpResponse("Hello Uriage.Py3 You're at the Uriage.")
 
