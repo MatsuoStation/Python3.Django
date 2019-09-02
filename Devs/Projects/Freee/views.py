f9ded93f0dcb38e42174bd626680a85b720088e8
VsV_MacBookPro2.9 <vsv_macbookpro2.9@VsV-MacBookPro29.local>
2019-09-02 17:05:41 +0900

    MatsuoSt.Py3.Dj : Devs.Freee.Py.Uriage_List.Views.CSV.Format - Ver.3.20.6 Modify
    Changes to be committed:
    	modified:   Projects/Freee/views.py
    	modified:   templates/uriage_list.html

--- Devs/Projects/Freee/views.py
+++ Devs/Projects/Freee/views.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|       "VsV.Py3.Dj.Freee.Views.py - Ver.3.20.5 Update:2019.09.02" |
+#//|       "VsV.Py3.Dj.Freee.Views.py - Ver.3.20.6 Update:2019.09.02" |
 #//+------------------------------------------------------------------+
 # rom django.shortcuts import render
 from django.shortcuts import get_object_or_404, render, redirect
@@ -63,5 +63,6 @@ def Uriage(request):
 	return render(request, 'uriage.html', {'form': form})
 
 
+### Index ###
 def index(request):
 	return HttpResponse("Hello Freee.Py3 You're at the Index.")
