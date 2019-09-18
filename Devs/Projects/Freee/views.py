4169d30a737fafb923f50401831e75978a4b0f7b
VsV_MacBookPro2.9 <vsv_macbookpro2.9@VsV-MacBookPro29.local>
2019-09-18 14:09:16 +0900

    MatsuoSt.Py3.Dj : Devs.Freee.Py.Views.CSV.RedCode.Target.List.Delete.Def - Ver.3.20.35 Modify
    Changes to be committed:
    	modified:   Projects/Freee/views.py
    	modified:   templates/uriage_csv.html

--- Devs/Projects/Freee/views.py
+++ Devs/Projects/Freee/views.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|      "VsV.Py3.Dj.Freee.Views.py - Ver.3.20.34 Update:2019.09.17" |
+#//|      "VsV.Py3.Dj.Freee.Views.py - Ver.3.20.35 Update:2019.09.18" |
 #//|               https://qiita.com/hujuu/items/b0339404b8b0460087f9 |
 #//|                https://qiita.com/mazu/items/77db19ca2caf128cc062 |
 #//|                            https://techacademy.jp/magazine/18994 |
@@ -201,12 +201,58 @@ class Uriage_CSV(ListView):
 
 				## K_*.CSV : Delete RedCord_*.CSV
 				### Pandas : CSV.読み取り ###
+				df_k = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'}, encoding='utf-8')
 				# df = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'}, index_col=0)
-				df_k = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'})
+
+				### Pandas : RedCode.CSV.読み取り ###
+				df_r = pd.read_csv("SHARP/K/K_RedCode_" + str(yid) + ".csv", sep=',', usecols=['C_Day','C_No','品目'], dtype={'管理番号':'object','C_No':'object'}, encoding='utf-8')
+				# df_r = pd.read_csv("SHARP/K/K_RedCode_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'}, encoding='utf-8')
 
 				### Pandas : 条件指定 ###
-				df_r = pd.read_csv("SHARP/K/K_RedCode_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'}, encoding='utf-8')
+				## RedCode.CSV : 行数
+				df_r_list_len = len(df_r)
+				context['df_r_list_len'] = df_r_list_len
 
+				## RedCode.CSV : 抽出
+				df_r_target = df_r.query('C_Day.astype("str").str.contains("201")', engine='python')
+				df_r_target_l = len(df_r_target)
+
+				context['df_r_target'] = df_r_target
+				context['df_r_target_len'] = df_r_target_l
+
+				# (NG)df_r_target['RC'] = pd.concat([df_r_target['C_Day'], df_r_target['C_No']])
+
+				# df_r_target_v = np.array(df_r_target['C_Day'].astype("str").str.split(','))
+				# df_r_target_v = np.append(df_r_target_v, [df_r_target['C_No']], axis=1)
+				# df_r_target['RC'] = df_r_target['C_Day'].astype("str").str.split(',')
+				# df_r_target['rCD'] = df_r_target["C_Day"].No.values.tolist()
+				# df_r_target['rCN'] = df_r_target["C_No"].values.tolist()
+				# df_r_target['rCD'] = list(df_r_target['C_Day'])
+				# df_r_target['rCN'] = list(df_r_target['C_No'])
+				# df_r_target['rCD'] = df_r_target['C_Day'].astype("str").str.split(',')
+				# df_r_target['rCN'] = df_r_target['C_No'].astype("str").str.split(',')
+				# df_r_target['RC'] = df_r_target['rCN']
+
+				# df_r_target['RC'] = df_r_target['RC'].extend(df_r_target['rCN'])
+
+				df_r_target_v = df_r_target.values.tolist()
+				# df_r_target_v = df_r_target.values
+				# df_r_target_v = np.array([range(df_r_list_v, df_r_list_v+2) for df_r_list_v])
+
+				# (OK) df_r_target_v = df_r_target['RC']
+				# df_r_target_v = df_r_target['C_Day'].apply(len)
+				# df_r_target_v = df_r_target['C_Day','C_No'].str.split(',')
+				# for row in df_r_target.itertuples():
+				#	df_r_target_v = [row[14]]
+				# for index, row in df_r_target.iterrows():
+				#	df_r_target_v = [row]
+				# for C_Day, C_No, 品目 in zip(df_r_target['C_Day'], df_r_target['C_No'], df_r_target['品目']):
+				# for C_Day in df_r_target['C_Day']:
+					# df_r_target_v = [C_Day,C_No,品目]
+
+				context['df_r_target_v'] = df_r_target_v
+
+				# (OK)
 				df_k_target_i = df_k.reset_index().query('発生日 == "2018-08-01" & 管理番号 == "0042" & 品目 == "10100"').index
 				# dfk = df.query('発生日 == "2018-08-01" & 管理番号 == "0042" & 品目 == "10100"')
 				# dfk = df
@@ -214,6 +260,7 @@ class Uriage_CSV(ListView):
 				## カラム : 型設定
 				# dfr['管理番号'].astype('str').str.zfill(4)
 				# dfr['C_No'].astype('str').str.zfill(4)
+
 
 				## 抽出
 				## (C_Day : True) : 自レコード取得
