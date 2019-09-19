7018a836150527258ef9d9b2bff03a290e47d1a3
VsV_MacBookPro2.9 <vsv_macbookpro2.9@VsV-MacBookPro29.local>
2019-09-19 15:52:12 +0900

    MatsuoSt.Py3.Dj : Devs.Freee.Py.Views.ALL.Uriage.List.Setup - Ver.3.20.40 Modify
    Changes to be committed:
    	modified:   Projects/Freee/views.py
    	modified:   templates/uriage_csv.html
    	modified:   templates/uriage_list.html

--- Devs/Projects/Freee/views.py
+++ Devs/Projects/Freee/views.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|      "VsV.Py3.Dj.Freee.Views.py - Ver.3.20.36 Update:2019.09.18" |
+#//|      "VsV.Py3.Dj.Freee.Views.py - Ver.3.20.40 Update:2019.09.19" |
 #//|               https://qiita.com/hujuu/items/b0339404b8b0460087f9 |
 #//|                https://qiita.com/mazu/items/77db19ca2caf128cc062 |
 #//|                            https://techacademy.jp/magazine/18994 |
@@ -155,222 +155,6 @@ class Uriage_CSV(ListView):
 		SHARP20_K = 'SHARP20_K_' + str(yid)
 		context['SHARP20_K'] = SHARP20_K
 
-		### CSV ###
-		## SSL
-		ssl._create_default_https_context = ssl._create_unverified_context
-
-		## URL
-		url = "https://dev.matsuostation.com/Freee/Uriage/" + str(yid) + "/"
-		html = urlopen(url)
-		# html = urlopen("https://dev.matsuostation.com/Freee/Uriage/" + str(yid) + "/")
-		# html = urlopen("https://dev.matsuostation.com/Freee/Uriage/2019/")
-		bsObj = BeautifulSoup(html, "html.parser")
-
-		## Table
-		# (OK) table = bsObj.findAll("table", {"class":"table"})[0]
-		# (OK) rows = table.findAll("tr")
-
-		## LastPage
-		# res = requests.get(url)
-		# res = requests.get(html, headers=Agent)
-
-		LastA = bsObj.findAll("div", {"class":"pagination"})[0]("a", {"class":"LastPage"})[0]["href"]
-		# (Main) LastPage = int(re.findall('page=([0-9]+)', LastA)[0])
-
-		# (Test)
-		LastPage = int(5)
-		# (OK) LastPage = re.findall('[0-9]+', LastA)[0]
-		# (OK) LastPage = re.findall('[0-9]+', LastA)
-		# LastPage = int(re.findAll(r'page=([0-9]+)[^<]*LastPage',res.text)[0])
-		# LastPage = re.search('page=([0-9]+)[^?]*', LastA)
-		# LastA = bsObj.findAll("pagination", {"class":"LastPage"})[0]["href"]
-
-		context['LastPage'] = LastPage
-
-		# if LastPage:
-			# context['LastPage'] = LastPage
-		# context['LastPage'] = print(LastPage)
-		# context['LastPage'] = LastA # ?pae=1093&dl=
-
-
-		## Scraping
-		## *.CSV : File_Check
-		if os.path.exists("SHARP/K/K_" + str(yid) + ".csv"):
-			context['CSV_Check'] = "True"
-
-			## RedCord_*.CSV : File_Check
-			if os.path.exists("SHARP/K/K_RedCode_" + str(yid) + ".csv"):
-				context['CSV_RedCord'] = "True"
-
-
-				## K_*.CSV : Delete RedCord_*.CSV
-				### Pandas : CSV.読み取り ###
-				df_k = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'}, encoding='utf-8')
-				# df = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'}, index_col=0)
-
-				### Pandas : RedCode.CSV.読み取り ###
-				df_r = pd.read_csv("SHARP/K/K_RedCode_" + str(yid) + ".csv", sep=',', usecols=['C_Day','C_No','品目'], dtype={'管理番号':'object','C_No':'object'}, encoding='utf-8')
-				# df_r = pd.read_csv("SHARP/K/K_RedCode_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'}, encoding='utf-8')
-
-				### Pandas : 条件指定 ###
-				## RedCode.CSV : 行数
-				df_r_list_len = len(df_r)
-				context['df_r_list_len'] = df_r_list_len
-
-				## RedCode.CSV : 抽出
-				df_r_target = df_r.query('C_Day.astype("str").str.contains("201")', engine='python')
-				context['df_r_target'] = df_r_target
-
-				df_r_target_l = len(df_r_target)
-				context['df_r_target_len'] = df_r_target_l
-
-				df_r_target_v = df_r_target.values.tolist()
-				context['df_r_target_v'] = df_r_target_v
-
-
-				df_k_target_i = []
-				# df_r_target_q = pd.DataFrame(df_r_target_v)
-				for x in df_r_target_v:
-					df_k_target_i.append(df_k.reset_index().query('発生日==@x[1] & 管理番号==@x[2] & 品目==@x[0]').index[0])
-
-				# dft = df_k.drop(df_k_target_i)
-				# dft.to_csv("SHARP/K/K_Target_Del_" + str(yid) + ".csv", encoding='utf-8', index=0)
-
-
-				# df_k_target_i = []
-				# for x in df_r_target_v:
-					# (OK) df_k_target_i.append(x[0])
-				#	df_k_target_i = df_k.query('発生日 == @df_k_target_i').index
-				# (OK) df_k_target_i = pd.DataFrame(df_r_target_v)
-				# df_k_target_q = pd.DataFrame(df_r_target_v)
-				# df_k_target_i = df_k_target_q()
-				# df_k_target_i = df_k.query("@df_r_target_v")
-				# df_k_target_i = df_k.reset_index().query('発生日 == ' + "2018-08-01" + ' & 管理番号 == ' + "0042" + ' & 品目 == ' + "10100").index
-				# df_k_target_i = df_k.reset_index(['C_Day','C_No']).index
-
-				# for tl in df_r_target_l:
-				#	df_k_target_i = df_r_target.values
-
-				# (OK) df_k_target_i = df_k.reset_index().query('発生日 == "2018-08-01" & 管理番号 == "0042" & 品目 == "10100"').index
-				# dfk = df.query('発生日 == "2018-08-01" & 管理番号 == "0042" & 品目 == "10100"')
-				# dfk = df
-
-				## カラム : 型設定
-				# dfr['管理番号'].astype('str').str.zfill(4)
-				# dfr['C_No'].astype('str').str.zfill(4)
-
-
-				## 抽出
-				## (C_Day : True) : 自レコード取得
-				# df_redcode_mine = df_redcode[df_redcode['C_Day'] != ""]
-				df_r_mine = df_k[df_k['C_Day'].astype('str').str.contains("201")]
-				context['df_r_mine'] = df_r_mine
-
-				df_r_mine_i = df_k[df_k['C_Day'].astype('str').str.contains("201")].index
-				# dfr_i = dfr[dfr['C_Day'].astype('str').str.contains('201')].index
-				# dfr_i = dfr[dfr['C_Day'].astype('str').str.contains('201')]
-				# (OK) dfr_i = dfr[dfr['C_Day'] != ""].index
-				# dfr_i = dfr.reset_index().index
-				context['df_r_mine_i'] = df_r_mine_i
-
-				dfd = df_k.drop(df_r_mine_i)
-				dfd.to_csv("SHARP/K/K_CNo_Del_" + str(yid) + ".csv", encoding='utf-8', index=0)
-
-				## (C_Day : False) : 対象レコード取得
-
-
-				# dfd = df.drop(12)
-				# (OK) dfd = df.drop(dfk)
-				# (OK) dfd.to_csv("SHARP/K/K_CNo_Del_" + str(yid) + ".csv", encoding='utf-8', index=0)
-				# dfd.to_csv("SHARP/K/K_R_" + str(yid) + ".csv", encoding='utf-8')
-				# dfk.to_csv("SHARP/K/K_R_" + str(yid) + ".csv", encoding='utf-8')
-
-
-				# print(df.reset_index().query('発生日 == "2018-08-01" & 管理番号 == "0042" & 品目 == "10100"').index[0])
-				# print(df.reset_index().query('発生日 == "2018-08-01" & 管理番号 == "0042" & 品目 == "10100"').index)
-				# print(dfk.reset_index())
-
-
-				context['df_k_target_i'] = df_k_target_i
-
-			else:
-				context['CSV_RedCord'] = "False"
-
-				### Pandas : CSV.RedCord ###
-				dfs = CSV_RedCord(yid)
-				context['dfs'] = dfs
-
-			''' (OK) Ver.3.20.30
-			df = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'}, index_col=0)
-			# dfr = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv").head(2)
-			# dfs = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv", header=1).head(2)
-			# dfs = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv", header=1).head(1)
-			# dfs = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv", names=('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R'))
-
-			# dfs = df[['発生日','管理番号','red_code']].query('red_code == 8')
-			# dfs = dfr[['収支区分','管理番号']].head() # 任意の列のみ.取得
-
-			df['管理番号'].astype('str').str.zfill(4)
-			df['C_No'].astype('str').str.zfill(4)
-
-			dfs = df[df['red_code'] == 8]
-			dfs.to_csv("SHARP/K/K_Red_" + str(yid) + ".csv", encoding='utf-8')
-
-			print(df.dtypes)
-
-			context['dfs'] = dfs
-			'''
-
-
-		else:
-			context['CSV_Check'] = "False"
-
-			with open("SHARP/K/K_" + str(yid) + ".csv", "w", encoding='utf-8') as file:
-				
-				### CSV.出力 ###
-				CSV_Write(file, LastPage, url)
-
-		# with open("SHARP/K/K_" + str(yid) + ".csv", "w", encoding='utf-8') as file:
-			# CSV_Write(file, LastPage, url)
-
-			''' (OK) Ver.3.20.23
-			wr = csv.writer(file)
-
-			for page in range(1, LastPage+1):
-
-				## URL.ReSetup
-				urlPage = url + "?page=" + str(page)
-				time.sleep(1)
-				htmlPage = urlopen(urlPage)
-				bsObjPage = BeautifulSoup(htmlPage, "html.parser")
-
-				## Table
-				table = bsObjPage.findAll("table", {"class":"table"})[0]
-
-				if page == 1:
-					rows = table.findAll("tr")
-
-				else:
-					rows = table.findAll("tr")[1:]
-
-				for row in rows:
-					csvRow = []
-					for cell in row.findAll(['td', 'th']):
-						csvRow.append(cell.get_text())
-					wr.writerow(csvRow)
-			'''
-
-		''' (OK) Ver.3.20.21
-		# with open("SHARP/K/K_2019.csv", "w", encoding='utf-8') as file:
-			wr = csv.writer(file)
-			for row in rows:
-				csvRow = []
-				for cell in row.findAll(['td', 'th']):
-					csvRow.append(cell.get_text())
-				wr.writerow(csvRow)
-		'''
-
-
 		return context
 
 
@@ -429,25 +213,20 @@ class Uriage_List(ListView):
 				# sql = "SELECT * FROM %s " % SHARP20_K + "WHERE m_datetime > '2018-08-01' AND p_code = %s AND r_code IN (%s,%s) ORDER BY m_datetime"
 				sql = "SELECT * FROM %s " % SHARP20_K + \
 					"WHERE m_datetime > '%s-08-01' " % (yid-1)  + \
-					"AND m_datetime < '%s-08-01' " % yid + "AND p_code = %s AND r_code IN (%s,%s) ORDER BY m_datetime"
+					"AND m_datetime < '%s-08-01' " % yid + "AND p_code = %s ORDER BY m_datetime"
+					#(OK.現金売上のみ) "AND m_datetime < '%s-08-01' " % yid + "AND p_code = %s AND r_code IN (%s,%s) ORDER BY m_datetime"
 
-				# cursor.execute(sql,("2018-07-03","10","0","1",))
-				# cursor.execute(sql,("2018-07-03","10","0","1",))
-				# cursor.execute(sql,("10","0","1",))
-				cursor.execute(sql,("10","0","1",))
+				## 売上高
+				cursor.execute(sql,("10"))
+				# (OK.現金売上のみ) cursor.execute(sql,("10","0","1",))
 
 				SQL_Data = dictfetchall(cursor)
-				# context['sqls'] = SQL_Data
-
-				# result = cursor.fetchall()
-				# print(result)
-				# context['sqls'] = result
 
 		finally:
 			conn.close()
 
 		### Pager ###
-		paginator = Paginator(SQL_Data, 60)
+		paginator = Paginator(SQL_Data, 10)
 		try:
 			page = int(self.request.GET.get('page'))
 		except:
@@ -457,7 +236,6 @@ class Uriage_List(ListView):
 			SQL_Data = paginator.page(page)
 		except(EmptyPage, InvalidPage):
 			SQL_Data = paginator.page(1)
-
 
 		context['sqls'] = SQL_Data
 
