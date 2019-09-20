7f95ccd68076f8de1221b118ae64e118cf233639
VsV_MacBookPro2.9 <vsv_macbookpro2.9@VsV-MacBookPro29.local>
2019-09-20 11:15:30 +0900

    MatsuoSt.Py3.Dj : Devs.Freee.Py.Views.ALL_RedCode.List.CSV.Def - Ver.3.20.42 Modify
    Changes to be committed:
    	modified:   Projects/Freee/views.py
    	modified:   templates/uriage_csv.html

--- Devs/Projects/Freee/views.py
+++ Devs/Projects/Freee/views.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|      "VsV.Py3.Dj.Freee.Views.py - Ver.3.20.41 Update:2019.09.19" |
+#//|      "VsV.Py3.Dj.Freee.Views.py - Ver.3.20.42 Update:2019.09.20" |
 #//|               https://qiita.com/hujuu/items/b0339404b8b0460087f9 |
 #//|                https://qiita.com/mazu/items/77db19ca2caf128cc062 |
 #//|                            https://techacademy.jp/magazine/18994 |
@@ -110,23 +110,23 @@ def CSV_Write(file, LastPage, url):
 ### CSV.RedCord ###
 def CSV_RedCord(yid):
 
-	### Pandas : CSV.読み取り ###
-	df = pd.read_csv("SHARP/K/K_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'}, index_col=0, encoding='utf-8')
+	### Pandas : ALL_K_*.CSV - 読み取り ###
+	df = pd.read_csv("SHARP/K/ALL_K_" + str(yid) + ".csv", sep=',', dtype={'管理番号':'object','C_No':'object'}, index_col=0, encoding='utf-8')
 
 	## カラム : 型設定
 	df['管理番号'].astype('str').str.zfill(4)
 	df['C_No'].astype('str').str.zfill(4)
 
 	## 抽出
-	dfs = df[df['red_code'] == 8]
+	df_redcode = df[df['red_code'] == 8]
 
 	## toCSV
-	dfs.to_csv("SHARP/K/K_RedCode_" + str(yid) + ".csv", encoding='utf-8')
+	df_redcode.to_csv("SHARP/K/ALL_RedCode_" + str(yid) + ".csv", encoding='utf-8')
 
 	## df.型出力
 	print(df.dtypes)
 
-	return dfs
+	return df_redcode
 
 
 ### Uriage_CSV ###
@@ -170,13 +170,27 @@ class Uriage_CSV(ListView):
 
 
 		### *.CSV - File_Check ###
-		## SHARP/K/K_*.CSV : True
-		if os.path.exists("SHARP/K/K_" + str(yid) + ".csv"):
-			context['CSV_Check'] = "True"
+		## SHARP/K/ALL_K_*.CSV : True
+		if os.path.exists("SHARP/K/ALL_K_" + str(yid) + ".csv"):
+			context['ALL_CSV_Check'] = "True"
 
-		## SHARP/K/K_*.CSV : False
+			## SHARP/K/ALL_RedCode_*.CSV : True
+			if os.path.exists("SHARP/K/ALL_RedCode_" + str(yid) + ".csv"):
+				context['ALL_RedCord_Check'] = "True"
+
+
+			## SHARP/K/RedCode.CSV : False
+			else:
+				context['ALL_RedCord_Check'] = "False"
+
+				## ALL_RedCode_*.CSV 出力
+				df_redcode = CSV_RedCord(yid)
+				context['df_redcode']
+
+
+		## SHARP/K/ALL_K_*.CSV : False
 		else:
-			context['CSV_Check'] = "False"
+			context['ALL_CSV_Check'] = "False"
 
 			## ALL_K_*.CSV 出力
 			with open("SHARP/K/ALL_K_" + str(yid) + ".csv", "w", encoding='utf-8') as file:
