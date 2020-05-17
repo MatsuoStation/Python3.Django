d07114196b466e6c134906997320dfb2c01686e8
Yoshio.Mr24 <sys.verysvery+github@gmail.com>
2020-05-17 09:43:29 +0900

    MatsuoSt.Py3.Dj : Devs.FreeeAPI.Py.Setup - Ver.3.50.1 Setup
    	new folder:   Projects/FreeeAPI
    	modified:   Projects/FreeeAPI/urls.py
    	modified:   Projects/FreeeAPI/views.py
    	modified:   config/urls.py

---
+++ Devs/Projects/FreeeAPI/views.py
@@ -0,0 +1,17 @@
+#//+------------------------------------------------------------------+
+#//|                    VerysVeryInc.Python3.Django.FreeeAPI.Views.py |
+#//|                  Copyright(c) 2018, VerysVery Inc. & Yoshio.Mr24 |
+#//|                 https://github.com/MatsuoStation/Python3.Django/ |
+#//|                                                 Since:2018.03.05 |
+#//|                                Released under the Apache license |
+#//|                       https://opensource.org/licenses/Apache-2.0 |
+#//|    "VsV.Py3.Dj.FreeeAPI.Views.py - Ver.3.50.1 Update:2020.05.17" |
+#//+------------------------------------------------------------------+
+from django.shortcuts import render
+
+# Create your views here.
+### MatsuoStation.Com ###
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Hello FreeeAPI.py. You're at the Index.")
