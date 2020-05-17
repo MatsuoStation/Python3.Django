13e1350c6fef5480fa071a5898016f7253a536f9
Yoshio.Mr24 <sys.verysvery+github@gmail.com>
2020-05-17 12:49:21 +0900

    MatsuoSt.Py3.Dj : Devs.FreeeAPI.views.Test.Def.Setup - Ver.3.50.4 modify
    Changes to be committed:
    	modified:   Projects/FreeeAPI/views.py

--- Devs/Projects/FreeeAPI/views.py
+++ Devs/Projects/FreeeAPI/views.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|    "VsV.Py3.Dj.FreeeAPI.Views.py - Ver.3.50.3 Update:2020.05.17" |
+#//|    "VsV.Py3.Dj.FreeeAPI.Views.py - Ver.3.50.4 Update:2020.05.17" |
 #//+------------------------------------------------------------------+
 from django.shortcuts import render
 
@@ -25,12 +25,17 @@ def Test(request):
 	# return HttpResponse("Test.Freee.API Page!! Welcome to Devs.MatsuoStation.Com!")
 
 	### Def.API.Authorization.Code
-	AUTHORIZATION_CODE = '6604a25e68356c9a4d3987a7a532d573b7e4cb50309db382aa04719f20171c68'
+	AUTHORIZATION_CODE = '5d9326f7c26e1b924a3337f5fac1ffd00bec9fd7caf57acf89da64271fd8ff51'
 
 	### (GET) FreeeConfig.Json
 	with open("../freeeconfig.json") as fc:
 		fc_data = json.load(fc)
 
+
+	# クライアントID
+	# CLIENT_ID = fc_data['client_id']
+	# クライアント・Secret
+	# CLIENT_SECRET = fc_data['client_secret']
 
 	# 事務所一覧.取得用API.URL
 	COMPANY_API_URL = fc_data['company_api_url']
@@ -56,11 +61,80 @@ def Test(request):
 	if REFRESH_TOKEN:
 		NEW_A_TOKEN = Get_A_Token(REFRESH_TOKEN, ACCESS_TOKEN)
 
+		# Freee.Session.SetUp
+		FreeeOAuth.headers['Authorization'] = 'Bearer ' + NEW_A_TOKEN
+
+		# Freee.Session.GET
+		rc = FreeeOAuth.get(COMPANY_API_URL)
+
+		# 正常受信 : 200
+		if rc.status_code == 200:
+			data_company = json.loads(rc.text)
+
+			COMPANY_ID = data_company['companies'][0]['id']
+
+			## 取引先一覧.取得.Params
+			params = {
+				'company_id'	: COMPANY_ID,
+				'offset'		: 0,
+				'limit'			: 500,
+				'keyword'		: "岡山"
+			}
+
+			# Freee.Partner.Session.GET
+			rp = FreeeOAuth.get(PARTNER_API_URL, params=params)
+
+			# 正常受信 : 200
+			if rp.status_code == 200:
+				data_partners = json.loads(rp.text)
+
+				Patners_List = data_partners['partners']
+				# Patners_List = data_partners['partners'][0]
+
+				P_List = list()
+				for item in Patners_List:
+					P_List.append(item['name'])
+				GET_Data = P_List
+
+			# 受信エラー : != 200
+			else:
+				ErrorCode = rp.status_code
+
+				GET_Data = ErrorCode
+
+
+			return render(request, 'freee_api.html', {
+						'R_Token'	: REFRESH_TOKEN,
+						'A_Token'	: NEW_A_TOKEN,
+						# 'C_ID'		: CLIENT_ID,
+						# 'C_Sec'		: CLIENT_SECRET,
+						'COMPANY_ID': COMPANY_ID,
+						'Data'		: GET_Data,
+					})
+
+
+		# 受信エラー : != 200
+		else:
+			ErrorCode = rc.status_code
+
+			GET_Data = ErrorCode
+
+			return render(request, 'freee_api.html', {
+						'R_Token'	: REFRESH_TOKEN,
+						'A_Token'	: NEW_A_TOKEN,
+						'Data'		: GET_Data,
+					})
+
+
+
+		'''
+		(Def)
 		return render(request, 'freee_api.html', {
 					'R_Token'	: REFRESH_TOKEN,
 					'A_Token'	: NEW_A_TOKEN,
 					'Data'		: fj_data,
 				})
+		'''
 
 	# REFRESH_TOKEN.False
 	else:
