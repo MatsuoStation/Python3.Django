cec46af77a36f535225c7a19d7f7bc32134b0dce
Yoshio.Mr24 <sys.verysvery+github@gmail.com>
2020-12-09 09:55:11 +0900

    MatsuoSt.Py3.Dj : Devs.nInvoice.MySQL.SHARPnPOS.Def - Ver.3.70.5 Modify
    Changes to be committed:
    	modified:   Projects/Finance/models.py

--- Devs/Projects/Finance/models.py
+++ Devs/Projects/Finance/models.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|   "VsV.Py3.Dj.Finance.Models.py - Ver.3.20.30 Update:2019.09.15" |
+#//|    "VsV.Py3.Dj.Finance.Models.py - Ver.3.70.5 Update:2020.12.09" |
 #//+------------------------------------------------------------------+
 from django.db import models
 
@@ -15,6 +15,61 @@ from django.conf import settings
 
 
 # settings.DATETIME_FORMAT
+
+
+###* SHARPnPOS *###  ( m_datetime : DateTimeField, g_code : ForeignKeyField, s_code : ForeignKeyField )
+class SHARPnPOS(models.Model):
+	class Meta:
+		db_table = 'SHARPnPOS'
+		verbose_name = 'SHARPnPOS'
+		verbose_name_plural = verbose_name
+		ordering = ['id']
+	id 		= models.IntegerField( verbose_name='id', unique=True, primary_key=True )	# max_length=4
+	m_datetime 	= models.DateTimeField( verbose_name='取引日時' )		# 0000-00-00 00:00
+	# m_day 	= models.IntegerField( verbose_name='取引日', default=0 )					# max_length=8,
+	# m_time	= models.IntegerField( verbose_name='時間', default=0 )						# max_length=4,
+	p_code	= models.CharField( verbose_name='処理区分', default=None, max_length=2 )
+	d_type	= models.CharField( verbose_name='データ種類', default=None, max_length=3 )
+	r_code	= models.IntegerField( verbose_name='現金/掛コード', default=0 )				# max_length=1,
+	ss_code	= models.CharField( verbose_name='SSコード', default=None, max_length=5 )
+
+	car_code= models.CharField( verbose_name='顧客車番', default=None, max_length=4 )
+	memo	= models.CharField( verbose_name='メモ', default=None, max_length=3 )
+	pw_code	= models.CharField( verbose_name='暗証番号', default=None, max_length=4 )
+	exp_day	= models.CharField( verbose_name='有効期限', default=None, max_length=6 )
+	staff	= models.CharField( verbose_name='担当コード', default=None, max_length=3 )
+	red_code= models.IntegerField( verbose_name='赤伝', default=0 )						# max_length=1,
+	slip	= models.CharField( verbose_name='伝票番号', default=None, max_length=4 )
+	pump	= models.CharField( verbose_name='ポンプ番号', default=None, max_length=3 )
+	nozzle	= models.IntegerField( verbose_name='ノズル番号', default=0 )					# max_length=1,
+	pro_memo= models.CharField( verbose_name='商品メモ', default=None, max_length=11 )
+	# product	= models.CharField( verbose_name='商品', default=None, max_length=5 )
+
+	amount	= models.FloatField( verbose_name='数量', default=0 )						# max_length=8,
+	unit	= models.FloatField( verbose_name='単価', default=0 )						# max_length=8,
+	value	= models.IntegerField( verbose_name='税別金額', default=0 )					# max_length=9,
+	tax_code= models.IntegerField( verbose_name='税区分', default=0 )					# max_length=1,
+	tax 	= models.IntegerField( verbose_name='消費税', default=0 )					# max_length=9,
+	oil_tax	= models.IntegerField( verbose_name='軽油税', default=0 )					# max_length=9,
+	# point	= models.IntegerField( verbose_name='ポイント', default=0 )					# max_length=7,
+	# pay_code= models.IntegerField( verbose_name='支払区分', default=0 )					# max_length=1,
+	# auth_no	= models.CharField( verbose_name='承認No', default=None, max_length=10 )
+	# allot	= models.IntegerField( verbose_name='割賦回数', default=0 )					# max_length=2,
+	# coupon	= models.IntegerField( verbose_name='クーポン割引区分', default=0 )			# max_length=1,
+	# dis_unit= models.IntegerField( verbose_name='割引単価', default=0 )					# max_length=5,
+	# d_value	= models.IntegerField( verbose_name='割引金額', default=0 )					# max_length=6,
+	c_day	= models.IntegerField( verbose_name='伝票年月日(修正)', default=0 )			# max_length=8,
+	c_no	= models.CharField( verbose_name='伝票No(修正)', default=None, max_length=4 )
+	# sub_data= models.CharField( verbose_name='サブデータ(修正)', default=None, max_length=8 )
+	# ss_dev	= models.CharField( verbose_name='SSコード(修正)', default=None, max_length=10 )
+	# name_id = models.ForeignKey('Name_Test', on_delete=models.CASCADE)
+
+	g_code	= models.IntegerField( verbose_name='顧客コード', default=None )
+	s_code	= models.CharField( verbose_name='商品', default=None, max_length=5 )
+
+	def __str__(self):
+		return self.s_code
+
 
 ###* SHARP20.K_2020 *### ( m_datetime : DateTimeField, g_code : ForeignKeyField, s_code : ForeignKeyField )
 class SHARP20_K_2020(models.Model):
