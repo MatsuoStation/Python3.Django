28048554c63bd56dcb19cb83ec5d3c4ebb3246f8
Yoshio.Mr24 <sys.verysvery+github@gmail.com>
2020-12-27 00:17:17 +0900

    MatsuoSt.Py3.Dj : Devs.vInvoice.Views.Invoice_Test20.Name_Test20 - Ver.3.80.4 Modify
    Changes to be committed:
    	modified:   Projects/vInvoice/views.py
    	modified:   templates/vInvoice.html
    	modified:   templates/vlist.html

--- Devs/Projects/vInvoice/views.py
+++ Devs/Projects/vInvoice/views.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|    "VsV.Py3.Dj.vInvoice.Views.py - Ver.3.80.3 Update:2020.12.26" |
+#//|    "VsV.Py3.Dj.vInvoice.Views.py - Ver.3.80.4 Update:2020.12.26" |
 #//+------------------------------------------------------------------+
 from django.shortcuts import render
 
@@ -15,6 +15,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import ListView
 
 from .forms import NameForm
+from Finance.models import Invoice_Test20, Name_Test20
 
 
 ### vInvoice_List ###
@@ -29,14 +30,21 @@ class vInvoice_List(ListView):
 		form = self.form_class(request.POST)
 		nid_post = request.POST['nid']
 		if form.is_valid():
-			return HttpResponseRedirect( '/vInvoice/%s' % nid_post )
+			return HttpResponseRedirect('/vInvoice/%s' % nid_post )
 		return render(request, self.template_name, {'form': form})
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
 
+		### Search : g_code ###
 		context['form'] = NameForm()
 		context['gid'] = self.kwargs.get('nid')
+
+		### DB : Setup ###
+		names = Invoice_Test20.objects.filter(g_code__uid=self.kwargs.get('nid')).select_related('g_code')
+
+		for name in names:
+			context['names'] = name.g_code.name
 
 		return context
 
