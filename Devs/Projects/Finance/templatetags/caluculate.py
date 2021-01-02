07667acc265ddbc51237a0830818c32a5e196849
Yoshio.Mr24 <sys.verysvery+github@gmail.com>
2021-01-02 17:27:36 +0900

    MatsuoSt.Py3.Dj : Devs.vInvoice.Cal.Unit_Cal.OIL.v30.Def - Ver.3.80.23 Modify
    Changes to be committed:
    	modified:   Projects/Finance/templatetags/caluculate.py

--- Devs/Projects/Finance/templatetags/caluculate.py
+++ Devs/Projects/Finance/templatetags/caluculate.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|     "VsV.Py3.Dj.TempTags.Cal.py - Ver.3.80.22 Update:2021.01.02" |
+#//|     "VsV.Py3.Dj.TempTags.Cal.py - Ver.3.80.23 Update:2021.01.02" |
 #//+------------------------------------------------------------------+
 from datetime import datetime
 from decimal import *
@@ -26,6 +26,63 @@ def Unit_Cal(sc, gc, am, vl, tax, red, md):
     if SC_Check(sc) == "Cash":
         sv, cTax = Cash_Cal(sc)
         uc = sv
+    # ハイオク(10000) or レギュラー(10100) or 軽油(10200) or 免税軽油(10300)
+    elif SC_Check(sc) == "OIL":
+        if Value_Test30.objects.all().filter(uid=gc, s_code=sc, m_datetime__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, m_datetime__lte=md), 0)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date01__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date01__lte=md), 1)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date02__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date02__lte=md), 2)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date03__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date03__lte=md), 3)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date04__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date04__lte=md), 4)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date05__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date05__lte=md), 5)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date06__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date06__lte=md), 6)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date07__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date07__lte=md), 7)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date08__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date08__lte=md), 8)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date09__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date09__lte=md), 9)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date10__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date10__lte=md), 10)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date11__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date11__lte=md), 11)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date12__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date12__lte=md), 12)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date13__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date13__lte=md), 13)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date14__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date14__lte=md), 14)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date15__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date15__lte=md), 15)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date16__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date16__lte=md), 16)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date17__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date17__lte=md), 17)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date18__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date18__lte=md), 18)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date19__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date19__lte=md), 19)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date20__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date20__lte=md), 20)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date21__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date21__lte=md), 21)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date22__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date22__lte=md), 22)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date23__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date23__lte=md), 23)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date24__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date24__lte=md), 24)
+        elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date25__lte=md):
+            uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date25__lte=md), 25)
+        # None OIL Value
+        else:
+            uc = sc
     # 油以外 : 灯油(10500) or 重油(10600)含む
     elif SC_Check(sc) == "nOIL":
         sv, cTax = nOIL_Cal(sc, vl, tax, jtax, red)
@@ -35,44 +92,39 @@ def Unit_Cal(sc, gc, am, vl, tax, red, md):
     # uc = sc
     return uc
 
-    '''
-    # uc = Unit_His(sc, gc, md)
-    if Value_Test30.objects.all().filter(uid=gc, s_code=sc, m_datetime__lte=md):
-        uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, m_datetime__lte=md), "now")
-        #v_values = Value_Test30.objects.all().filter(uid=gc, s_code=sc, m_datetime__lte=md)
-        #for v in v_values:
-        #    uc = v.value
-    elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date01__lte=md):
-        uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date01__lte=md), "date01")
-    elif Value_Test30.objects.all().filter(uid=gc, s_code=sc, date02__lte=md):
-        uc = Unit_His(Value_Test30.objects.all().filter(uid=gc, s_code=sc, date02__lte=md), "date02")
-    else:
-        # 現金関係 or 小切手関係 or 振込関係 or 相殺関係 or 売掛回収
-        if SC_Check(sc) == "Cash":
-            sv, cTax = Cash_Cal(sc)
-            uc = sv
-        else:
-            uc = sc
-        # OIL以外
-        #elif SC_Check(sc) == "nOIL":
-        #    jtax = jTax(md)
-        #    sv, cTax = nOIL_Cal(sc, vl, tax, jtax, red)
-        #    uc = Decimal(sv / (am / 100)).quantize(Decimal('1'), rounding=ROUND_DOWN)
-    return uc
-    '''
 
 ### 単価 : SS.History ###
 def Unit_His(v30, hv):
     v_values = v30
     for v in v_values:
-        if hv == "now":
-            uh = v.value
-        elif hv == "date01":
-            uh = v.value01
-        elif hv == "date02":
-            uh = v.value02
-        else:
-            uh = 9999999
+        if hv == 0: uh = v.value
+        elif hv == 1: uh = v.value01
+        elif hv == 2: uh = v.value02
+        elif hv == 3: uh = v.value03
+        elif hv == 4: uh = v.value04
+        elif hv == 5: uh = v.value05
+        elif hv == 6: uh = v.value06
+        elif hv == 7: uh = v.value07
+        elif hv == 8: uh = v.value08
+        elif hv == 9: uh = v.value09
+        elif hv == 10:uh = v.value10
+        elif hv == 11:uh = v.value11
+        elif hv == 12:uh = v.value12
+        elif hv == 13:uh = v.value13
+        elif hv == 14:uh = v.value14
+        elif hv == 15:uh = v.value15
+        elif hv == 16:uh = v.value16
+        elif hv == 17:uh = v.value17
+        elif hv == 18:uh = v.value18
+        elif hv == 19:uh = v.value19
+        elif hv == 20:uh = v.value20
+        elif hv == 21:uh = v.value21
+        elif hv == 22:uh = v.value22
+        elif hv == 23:uh = v.value23
+        elif hv == 24:uh = v.value24
+        elif hv == 25:uh = v.value25
+
+        else: uh = 99999999
     return uh
 
 ### 消費税率 : 2019/10/01 => 10%, 2014/4/1 => 8%
