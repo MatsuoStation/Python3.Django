ed1827f331858404adbc4901b88462375fa9cf05
Yoshio.Mr24 <sys.verysvery+github@gmail.com>
2021-01-06 11:25:53 +0900

    MatsuoSt.Py3.Dj : Devs.vInvoice.Views.PDF.New.Seikyu.03.PNG.Def - Ver.3.80.46 Modify
    Changes to be committed:
    	modified:   ../.gitignore
    	modified:   Projects/vInvoice/Util/pdf.py
    	modified:   templates/pdf_vlist.html

--- Devs/Projects/vInvoice/Util/pdf.py
+++ Devs/Projects/vInvoice/Util/pdf.py
@@ -5,15 +5,18 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|"VsV.Py3.Dj.vInvoice.Util.PDF.py - Ver.3.80.41 Update:2021.01.05" |
+#//|"VsV.Py3.Dj.vInvoice.Util.PDF.py - Ver.3.80.46 Update:2021.01.06" |
 #//+------------------------------------------------------------------+
 
 def fPDF_SS_BackImage(fPDF):
     if fPDF == 0:
-        fURL = "https://dev.matsuostation.com/static/images/Invoice/New_Seikyu_SS_0_02.png"
+        fURL = "https://dev.matsuostation.com/static/images/Invoice/New_Seikyu_SS_0_03.png"
+        # fURL = "https://dev.matsuostation.com/static/images/Invoice/New_Seikyu_SS_0_02.png"
     if fPDF == 10:
-        fURL = "https://dev.matsuostation.com/static/images/Invoice/New_Seikyu_SS_10_02.png"
+        fURL = "https://dev.matsuostation.com/static/images/Invoice/New_Seikyu_SS_10_03.png"
+        # fURL = "https://dev.matsuostation.com/static/images/Invoice/New_Seikyu_SS_10_02.png"
     if fPDF == 20:
-        fURL = "https://dev.matsuostation.com/static/images/Invoice/New_Seikyu_SS_20_02.png"
+        fURL = "https://dev.matsuostation.com/static/images/Invoice/New_Seikyu_SS_20_03.png"
+        # fURL = "https://dev.matsuostation.com/static/images/Invoice/New_Seikyu_SS_20_02.png"
 
     return fURL
