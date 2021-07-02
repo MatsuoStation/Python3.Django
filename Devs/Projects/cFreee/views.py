6ea0d7db004699482364cbab6653ae9e34025c24
Yoshio.Mr24 <sys.verysvery+github@gmail.com>
2021-07-02 09:17:26 +0900

    MatsuoSt.Py3.Dj : Devs.cFreee.GoogleAPI.gSpread.JsonFile.Setup - Ver.3.92.4 Modify
    Changes to be committed:
    	modified:   Projects/cFreee/views.py

--- Devs/Projects/cFreee/views.py
+++ Devs/Projects/cFreee/views.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|      "VsV.Py3.Dj.cFreee.Views.py - Ver.3.92.3 Update:2021.06.16" |
+#//|      "VsV.Py3.Dj.cFreee.Views.py - Ver.3.92.4 Update:2021.06.16" |
 #//+------------------------------------------------------------------+
 from django.shortcuts import render
 
@@ -28,10 +28,13 @@ def python(request):
 	# ws = connect_gspread(gJsonFile, spsh_key)
 	# spsh_url = "https://docs.google.com/spreadsheets/d/186lVOriJXzl_7wIlLxq7jgAxvrGtCq5YtdprnUpdGi4/edit#gid=0"
 	# ws = connect_gspread(gJsonFile, spsh_url)
+	ws_list = ws.worksheets()
 
-	ws.update_cell(1,1,"Test_3.92.2")
 
-	return HttpResponse("Google.Json.File = %s : %s" % (ws.title, ws.id))
+	# ws.update_cell(1,1,"Test_3.92.2")
+
+	return HttpResponse("Google.Json.File = %s : %s , %s" % (len(ws_list), ws_list[0].title, ws_list[1].title) )
+	# return HttpResponse("Google.Json.File = %s : %s" % (ws.title, ws.id))
 	# return HttpResponse("Hello cFreee/Python You're at the Python.")
 
 
@@ -44,7 +47,8 @@ def connect_gspread(jsonf, spsh):
 	SPREADSHEET_KEY = spsh
 	# SPREADSHEET_KEY = key
 
-	worksheet = gc.open(SPREADSHEET_KEY).sheet1
+	worksheet = gc.open(SPREADSHEET_KEY)
+	# worksheet = gc.open(SPREADSHEET_KEY).sheet1
 	# worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
 	# worksheet = gc.open_by_url(SPREADSHEET_KEY).sheet1
 
