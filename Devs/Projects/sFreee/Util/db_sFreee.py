7c169ec152428fde1826d1cd0b194a50fbc5ed57
Yoshio.Mr24 <sys.verysvery+github@gmail.com>
2021-09-30 01:35:17 +0900

    MatsuoSt.Py3.Dj : Devs.sFreee.GASAPI.gSpread.List.NoCal.+SnP20.Def - Ver.3.93.20 Modify
    Changes to be committed:
    	modified:   Projects/sFreee/Util/db_sFreee.py
    	modified:   Projects/sFreee/views.py

--- Devs/Projects/sFreee/Util/db_sFreee.py
+++ Devs/Projects/sFreee/Util/db_sFreee.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|   "VsV.Py3.Dj.Util.DB.sFreee.py - Ver.3.93.10 Update:2021.09.28" |
+#//|   "VsV.Py3.Dj.Util.DB.sFreee.py - Ver.3.93.20 Update:2021.09.30" |
 #//+------------------------------------------------------------------+
 ### MatsuoStation.Com ###
 # import django
@@ -17,8 +17,9 @@ from django.db.models import Q
 ### DB_sFreee ###
 def DB_sFreee(self, dlb, dla):
     ## p_code : 01.POS開始 / 10.00.''.売上高 / 20.売上回収 / 30.現金仕入 / 31.掛仕入 / 32.仕入返品 / 40.出金 / 50.棚卸 / 91.92.93.POS締
-    SnPs = SHARPnPOS_1501_2107.objects.filter((Q(p_code='10') | Q(p_code='') | Q(p_code='00')), m_datetime__gte=dlb, m_datetime__lte=dla).order_by('m_datetime')
-    SnP20 = SHARPnPOS_1501_2107.objects.filter(Q(p_code='20'), m_datetime__gte=dlb, m_datetime__lte=dla).order_by('m_datetime')
+    SnPs = SHARPnPOS_1501_2107.objects.filter((Q(p_code='10') | Q(p_code='20') | Q(p_code='') | Q(p_code='00')), m_datetime__gte=dlb, m_datetime__lte=dla).order_by('m_datetime')
+    # SnPs = SHARPnPOS_1501_2107.objects.filter((Q(p_code='10') | Q(p_code='') | Q(p_code='00')), m_datetime__gte=dlb, m_datetime__lte=dla).order_by('m_datetime')
+    # SnP20 = SHARPnPOS_1501_2107.objects.filter(Q(p_code='20'), m_datetime__gte=dlb, m_datetime__lte=dla).order_by('m_datetime')
     SnP30 = SHARPnPOS_1501_2107.objects.filter((Q(p_code='30') | Q(p_code='31') | Q(p_code='32')), m_datetime__gte=dlb, m_datetime__lte=dla).order_by('m_datetime')
     SnP40 = SHARPnPOS_1501_2107.objects.filter(Q(p_code='40'), m_datetime__gte=dlb, m_datetime__lte=dla).order_by('m_datetime')
     SnP50 = SHARPnPOS_1501_2107.objects.filter(Q(p_code='50'), m_datetime__gte=dlb, m_datetime__lte=dla).order_by('m_datetime')
@@ -28,4 +29,5 @@ def DB_sFreee(self, dlb, dla):
     # for q in SHARPnPOS_1501_2107.objects.all().filter(m_datetime__gte=dlb):
     #     SnPs = q
 
-    return SnPs, SnP20, SnP30, SnP40, SnP50, SnP90
+    return SnPs, SnP30, SnP40, SnP50, SnP90
+    # return SnPs, SnP20, SnP30, SnP40, SnP50, SnP90
--- Devs/Projects/sFreee/views.py
+++ Devs/Projects/sFreee/views.py
@@ -5,7 +5,7 @@
 #//|                                                 Since:2018.03.05 |
 #//|                                Released under the Apache license |
 #//|                       https://opensource.org/licenses/Apache-2.0 |
-#//|     "VsV.Py3.Dj.sFreee.Views.py - Ver.3.93.12 Update:2021.09.29" |
+#//|     "VsV.Py3.Dj.sFreee.Views.py - Ver.3.93.20 Update:2021.09.30" |
 #//+------------------------------------------------------------------+
 from django.shortcuts import render
 
@@ -31,6 +31,189 @@ from .Util.db_sFreee import DB_sFreee
 
 ### Google Apps Script ###
 class GAS(ListView):
+	model = SHARPnPOS_1501_2107
+	form_class = DateForm
+	template_name = 'sFreeeList.html'
+
+	def post(self, request, *args, **kwargs):
+		form = self.form_class(request.POST)
+		mdate_post = request.POST['mdate']
+		if form.is_valid():
+			return HttpResponseRedirect('/sFreee/GAS/%s' % mdate_post)
+		return render(request, self.template_name, {'form': form})
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		## GAS : SpreadSheet - Setup ##
+		spsh_name = "SS_U"
+		ws = connect_gspread(spsh_name)
+		ws_list = ws.worksheets()
+
+		## Search : m_datetime ##
+		context['form'] = DateForm()
+		if self.kwargs.get('mdate'):
+			mdate = self.kwargs.get('mdate')
+		else:
+			mdate = '2000-01'
+
+		## * try: * dl = Ture ##
+		try:
+			## 期間設定 ##
+			dl = mdate + '-01'
+			dlstr = datetime.strptime(dl, '%Y-%m-%d')
+
+			# dd = dlstr.day  # DeadLine : Day
+			dlb, dla = DeadLine(dlstr)
+
+			## DB : Setup ##
+			SnPs, SnP30, SnP40, SnP50, SnP90 = DB_sFreee(self, dlb, dla)
+			# SnPs, SnP20, SnP30, SnP40, SnP50, SnP90 = DB_sFreee(self, dlb, dla)
+			# SnPs = DB_sFreee(self, dlb, dla)
+
+		## * end try: * dl = False ##
+		except Exception as e:
+			print("Exception - views.py / dl=False  : %s" % e)
+
+			## 期間設定 ##
+			dl = mdate + '-01'
+			dlstr = datetime.strptime(dl, '%Y-%m-%d')
+
+			# dd = dlstr.day  # DeadLine : Day
+			dlb, dla = DeadLine(dlstr)
+
+			## DB : Setup ##
+			SnPs, SnP30, SnP40, SnP50, SnP90 = DB_sFreee(self, dlb, dla)
+			# SnPs, SnP20, SnP30, SnP40, SnP50, SnP90 = DB_sFreee(self, dlb, dla)
+			# SnPs = DB_sFreee(self, dlb, dla)
+
+		context['mdate'] = mdate
+		context['dlb'] = dlb
+		context['dla'] = dla
+
+		## GAS : 初期データ設定 ##
+		SnPs_count = len(SnPs)
+		context['snps_count'] = SnPs_count
+		Pager_count = 50;
+		SnPsVs = SnPs
+
+		## Paginator : Setup ##
+		paginator = Paginator(SnPs, Pager_count)
+		try:
+			page = int(self.request.GET.get('page'))
+		except:
+			page = 1
+		try:
+			SnPs = paginator.page(page)
+		except(EmptyPage, InvalidPage):
+			SnPs = paginator.page(1)
+		context['snps'] = SnPs
+
+		## GAS : データ設定 ##
+		snp_list = list()
+		try:
+			Pager_Page = SnPs.number
+			Pager_LastPage = math.ceil(SnPs_count / Pager_count)
+		except Exception as e:
+			print("Exception - Pager_page : %s" % e)
+
+		c = 0
+		for snpv in SnPs:
+		# for snpv in SnPsVs:
+
+			### GAS : 初期設定（ページ数)
+			if Pager_Page == 1:
+				p = 0
+			else:
+				p = ((Pager_Page - 1) * 50)
+
+			### GAS : 初期設定（入力データ)
+			pName = '=IFERROR(IF(Y' + str(c + 2 + p) + '<>"",VLOOKUP(VLOOKUP(Y' + str(c + 2 + p) + ',IMPORTRANGE("1gFItc1Ta3hXS1Ad6jEUjMfgNdU_y8ozHGuX_tmW1u5Q","PartnersList!D2:E"),2,false),IMPORTRANGE("1gFItc1Ta3hXS1Ad6jEUjMfgNdU_y8ozHGuX_tmW1u5Q","PartnersList!A2:C"),3,false),""))'
+			pItem = '=IFERROR(IF(AB' + str(c + 2 + p) + '<>"",VLOOKUP(VLOOKUP(AB' + str(c + 2 + p) + ',IMPORTRANGE("1vBG3-DMn8BANxhboZ2JkdzmNZm-DITy9-UPg9ID5bs8","ItemsList!D2:E"),2,false),IMPORTRANGE("1vBG3-DMn8BANxhboZ2JkdzmNZm-DITy9-UPg9ID5bs8","ItemsList!A2:C"),3,false),""))'
+			tax_section = '=IF(A' + str(c + 2 + p) + '<>"",1,0)'
+
+			'''
+			if snpv.amount != 0:
+				pAm = (snpv.amount / 100)
+				oAm = '=IFERROR(IF(AK' + str(c + 2 + p) + '=8,-(' + str(pAm) + '),' + str(pAm) + '))'
+
+				if snpv.value != 0:
+					oUnit = (snpv.value / pAm)
+					oVl = '=IFERROR(IF(AK' + str(c + 2 + p) + '=8,-(' + str(snpv.value) + '),' + str(snpv.value) + '))'
+					oTax = '=IFERROR(IF(AK' + str(c + 2 + p) + '=8,-(' + str(snpv.tax) + '),' + str(snpv.tax) + '))'
+				else:
+					oUnit = 0
+					oVL = 0
+					oTax = 0
+			else:
+				pAm = 0
+				oAm = 0
+				oUnit = 0
+				oVL = 0
+				oTax = 0
+			'''
+
+
+			p_r_code = str(snpv.p_code)+'/'+str(snpv.r_code)
+			if p_r_code == '10/0' or p_r_code == '10/1' or p_r_code == '20/9': pRcode = '現金'
+			else: pRcode = ''
+
+			## GAS : 赤伝先
+			if snpv.c_day != 0: oCC = str(snpv.c_day)+'/'+str(snpv.c_no)
+			else: oCC = ""
+
+
+			# oAllCash = '=IFERROR(IF(O'+str(c+2+p)+'="現金",AH'+str(c+2+p)+',""))'
+
+			### GAS : リスト設定
+			''' CSV項目 : / 収支区分, 管理番号, 発生日, 決済期日, 取引先, 勘定科目, 税区分, 金額, 税計算区分, 税額, 備考, 品目, 部門, \
+				(N) 決済日, 決済口座, 決済金額, Bank.ID, Row, \
+				(S) Deal.ID, Pay.ID, W.Type, W.ID, \
+				(W) 部門.ID, 取引先ID, g_code, 勘定科目ID, Tax.ID, Item.ID, 未支金額, 決済状況, \
+				(AE) 数量, 新単価, 旧単価, 旧金額, 旧税額, 新旧差額, 赤伝 '''
+			snp_list.append(['収入', snpv.slip, snpv.m_datetime.strftime('%Y-%m-%d'), '', pName, '売上高', '課税売上8%', 'snpv.value', '内税', 'snpv.tax', oCC, pItem, 'SS関係', \
+							 '', pRcode,'', 'BankID', '' , \
+							 'DealID', '', '', '', \
+							 '', '', snpv.g_code, '', '', snpv.s_code, '', '', \
+							 'oAm', '', 'oUnit', 'oVl', 'oTax', '', snpv.red_code])
+			c += 1
+			# print(c)
+
+		df = pd.DataFrame(snp_list)
+		print(df)
+		print(str(len(df)))
+		print('%s / %s' % (Pager_Page, Pager_LastPage))
+
+		## GAS : シート追加 ##
+		newShName = dl[2:-2].replace('-', '')
+		ws_list_value = 0
+
+		for i in range(len(ws_list)):
+			if ws_list[i].title == newShName:
+				ws_list_value = ws_list_value + 1
+		ws_list_cpid = ws_list[0].id
+
+		if ws_list_value == 0:
+			ws.duplicate_sheet(source_sheet_id=ws_list_cpid, new_sheet_name=newShName, insert_sheet_index=len(ws_list))
+
+		## GAS : 行数追加 ##
+		write_ws = ws.worksheet(newShName)
+		write_ws.add_rows(Pager_count)
+		# write_ws.add_rows(SnPs_count)
+		context['write_ws'] = newShName
+
+		## GAS : リスト書き込み　##
+		for index, record in df.iterrows():
+			cell_list = write_ws.range(index + 2 + p, 1, index + 2 + p, len(record))
+			for cell, val in zip(cell_list, record):
+				cell.value = val
+			write_ws.update_cells(cell_list, value_input_option='USER_ENTERED')
+			# write_ws.update_cells(cell_list)
+
+		return context
+
+
+class GAS_Bak(ListView):
 	model = SHARPnPOS_1501_2107
 	form_class = DateForm
 	# model = Name_Test20
